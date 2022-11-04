a156efe1ce7e330e826f7cd0b28d1758355be74e
MuammerKrc <muammer03karaca@gmail.com>
2022-11-04 03:36:35 +0300

    Service layer base done

---
+++ HealthApiSolution/ServiceLayer/ServiceRegistration.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using CoreLayer.ConfigurationModels;
+using CoreLayer.IServices;
+using CoreLayer.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ServiceLayer.Mapping;
+using ServiceLayer.Services;
+
+namespace ServiceLayer
+{
+    public static class ServiceRegistration
+    {
+        public static void AddServiceLayerRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            // auto mapper registered
+            services.AddAutoMapper(typeof(Mapper));
+
+
+            // configuration
+            services.Configure<TokenConfigurationModel>(configuration.GetSection("Token"));
+
+            //service registered
+            services.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IMovieService, MovieService>();
+            services.AddScoped<IFavoriteMoviesService, FavoriteMoviesService>();
+
+        }
+    }
+}
---
+++ HealthApiSolution/ServiceLayer/Services/CategoryService.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using CoreLayer.Dtos;
+using CoreLayer.Dtos.CategoryDtos;
+using CoreLayer.IRepositories;
+using CoreLayer.IServices;
+using CoreLayer.IUnitOfWorks;
+using CoreLayer.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace ServiceLayer.Services
+{
+    public class CategoryService : ICategoryService
+    {
+        private IUnitOfWork _unitOfWork;
+        private IMapper _mapper;
+
+
+        public CategoryService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<Response<IEnumerable<CategoryDto>>> GetAllCategory()
+        {
+            var response = await _unitOfWork.CategoryRepository.GetAllAsync();
+
+            return Response<IEnumerable<CategoryDto>>.SuccessResponse(_mapper.Map<List<CategoryDto>>(response));
+        }
+
+        public async Task<Response<CategoryDto>> GetCategoryWithMovieById(int id)
+        {
+            var response = await _unitOfWork.CategoryRepository.GetContext().Where(i => i.Id == id)
+                .Include(i => i.Movies).Select(x=>new Category()
+                {
+                    Movies = x.Movies,
+                    Id = x.Id,
+                    SoftDeleted = x.SoftDeleted,
+                    CreatedDate = x.CreatedDate,
+                    Title = x.Title,
+                    UpdateDate = x.UpdateDate
+                }).AsNoTracking().FirstOrDefaultAsync();
+            
+            return Response<CategoryDto>.SuccessResponse(_mapper.Map<CategoryDto>(response));
+        }
+
+        public async Task<Response<NoResponse>> CreateCategory(CategoryDto category)
+        {
+            var categoryModel = _mapper.Map<Category>(category);
+            _unitOfWork.CategoryRepository.Add(categoryModel);
+            await _unitOfWork.SaveAsync();
+            return Response<NoResponse>.SuccessResponse();
+        }
+    }
+}
