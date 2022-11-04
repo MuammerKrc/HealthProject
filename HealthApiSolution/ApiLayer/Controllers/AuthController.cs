8c73d171ba6d5cabee47e3191d1ebb21c7939c28
MuammerKrc <muammer03karaca@gmail.com>
2022-11-04 16:55:20 +0300

    added Health Client Solution

---
+++ HealthApiSolution/ApiLayer/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using CoreLayer.Dtos.IdentityDtos;
+using CoreLayer.IServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginDto login)
+        {
+            var result = await _authService.LoginAsync(login);
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> LoginWithRefreshToken(string refreshToken)
+        {
+            var result = await _authService.RefreshTokenLoginAsync(refreshToken);
+            return Ok(result);
+        }
+    }
+}
---
+++ HealthApiSolution/ApiLayer/Controllers/UsersController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiLayer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+    }
+}
