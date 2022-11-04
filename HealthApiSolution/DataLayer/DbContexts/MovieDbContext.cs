e9fc6a318351c35686fd83361dac795fe5196a92
MuammerKrc <muammer03karaca@gmail.com>
2022-11-04 03:22:54 +0300

    data layer base done

---
+++ HealthApiSolution/DataLayer/DbContexts/MovieDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoreLayer.Models;
+using CoreLayer.Models.BaseModels;
+using CoreLayer.Models.IdentityModels;
+using CoreLayer.Models.JwtModels;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataLayer.DbContexts
+{
+    public class MovieDbContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+
+        public MovieDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<FavoriteMovies> FavoriteMovies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<AppUser>().HasOne(i => i.FavoriteMovies).WithOne(i => i.User)
+                .HasForeignKey<FavoriteMovies>(i => i.UserId);
+
+            base.OnModelCreating(builder);
+        }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+        {
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ HealthApiSolution/DataLayer/ServiceRegistration.cs
@@ -0,0 +1,52 @@
+﻿using CoreLayer.IRepositories;
+using CoreLayer.IRepositories.IBaseRepositories;
+using CoreLayer.Models.IdentityModels;
+using DataLayer.DbContexts;
+using DataLayer.Repositories;
+using DataLayer.Repositories.BaseRepositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using CoreLayer.ConfigurationModels;
+using CoreLayer.IUnitOfWorks;
+using DataLayer.UnitOfWorks;
+using Microsoft.AspNetCore.Identity;
+
+namespace DataLayer
+{
+    public static class ServiceRegistration
+    {
+        public static void AddDataLayerRegistration(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<MovieDbContext>(i =>
+            {
+                i.UseSqlServer(configuration.GetConnectionString("DefaultConnection"), opt =>
+                {
+                    opt.MigrationsAssembly("DataLayer");
+                });
+            });
+
+            services.AddIdentity<AppUser, AppRole>(opt =>
+            {
+                opt.User.RequireUniqueEmail = true;
+                opt.Password.RequireDigit = false;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 5;
+                opt.Password.RequiredUniqueChars = 3;
+            }).AddEntityFrameworkStores<MovieDbContext>().AddDefaultTokenProviders();
+
+            //UnitOfWorks
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            //repository registered
+            services.AddScoped(typeof(IBaseRepository<,>), typeof(BaseRepository<,>));
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IFavoriteMoviesRepository, FavoriteMoviesRepository>();
+            services.AddScoped<IMovieRepository, MovieRepository>();
+
+        }
+    }
+}
